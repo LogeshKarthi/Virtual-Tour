bebf7d1ec23cb8d245ed1f706f87a15a9c7e1542
Logesh karthick <kskarthick21@gmail.com>
2018-06-29 15:46:49 +0530

    First commit

---
+++ News_paper/Assets/script/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void move(){
+
+		transform.localPosition += new Vector3 (0, 10, 0);
+		transform.eulerAngles += new Vector3 (5, 20, 5);
+	}
+}
---
+++ News_paper/Assets/script/GoBack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBack : MonoBehaviour {
+
+	// Use this for initialization
+	public void Back(int index)
+	{
+		SceneManager.LoadScene (index);
+	}
+}
